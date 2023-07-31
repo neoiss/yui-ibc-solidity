7af49324d59eb0e1f148fc3fb203b27175fd8238
neoiss <neoiss@163.com>
2023-07-31 08:32:06 +0800

    add revert msg

--- contracts/lib/Bytes.sol
+++ contracts/lib/Bytes.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.9;
 
 library Bytes {
     function toAddress(bytes memory bz) internal pure returns (address addr) {
-        require(bz.length == 20, "cannot convert into address");
+        require(bz.length == 20, "toAddress: cannot convert into address");
         assembly {
             addr := mload(add(bz, 20))
         }
     }
 
     function toBytes32(bytes memory bz) internal pure returns (bytes32 ret) {
-        require(bz.length == 32, "cannot convert into address");
+        require(bz.length == 32, "toBytes32: cannot convert into address");
         assembly {
             ret := mload(add(bz, 32))
         }
